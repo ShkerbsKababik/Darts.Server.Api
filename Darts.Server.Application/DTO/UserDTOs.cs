1de2fc162f58ca1b3a62b2f8578beb5d184e59e2
Vladimir <vovanov1901@mail.ru>
2025-07-19 15:56:24 +0300

    nya)

---
+++ Darts.Server.Application/DTO/UserDTOs.cs
@@ -0,0 +1,30 @@
+﻿namespace Darts.Server.Application.DTO;
+
+public class UserDTO
+{ 
+    public string Login { get; set; }
+}
+
+public class UserCreationDTO
+{
+    public string Login { get; set; }
+    public string Password { get; set; }
+}
+
+public class ChangeUserPasswordDTO
+{
+    public Guid UserId { get; set; }
+    public string Password { get; set; }
+}
+
+public class AddRolesToUserDTO
+{ 
+    public Guid UserId { get; set; }
+    public List<Guid> RolesIds { get; set; }
+}
+
+public class RemoveRolesFromUser
+{
+    public Guid UserId { get; set; }
+    public List<Guid> RolesIds { get; set; }
+}
---
+++ Darts.Server.Application/Services/UserService.cs
@@ -0,0 +1,86 @@
+﻿using Darts.Server.Application.DTO;
+using Darts.Server.Domain.Enatities;
+using Darts.Server.Domain.Interfaces;
+
+namespace Darts.Server.Application.Services;
+
+public interface IUserService
+{
+    void CreateUser(UserCreationDTO userCreationDTO);
+    void ChangeUserPassword(ChangeUserPasswordDTO changeUserPasswordDTO);
+    void AddRolesToUser(AddRolesToUserDTO addRolesToUserDTO);
+    void RemoveRolesToUser(RemoveRolesFromUser removeRolesFromUserDTO);
+}
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IRoleRepository _roleRepository;
+
+    public UserService(
+        IUserRepository userRepository, 
+        IRoleRepository roleRepository)
+    {
+        _userRepository = userRepository;
+        _roleRepository = roleRepository;
+    }
+
+    public void AddRolesToUser(AddRolesToUserDTO addRolesToUserDTO)
+    {
+        var user = _userRepository.GetUserById(addRolesToUserDTO.UserId);
+        if (user is null)
+        {
+            // throw exception
+        }
+
+        var roles = _roleRepository.GetAllRoles()
+            .Where(role => addRolesToUserDTO.RolesIds.Contains(role.Id))
+            .ToList();
+        if (roles.Count != addRolesToUserDTO.RolesIds.Count)
+        { 
+            // throw exception
+        }
+
+        user.AddRoles(roles);
+        _userRepository.UpdateUser(user);
+    }
+
+    public void ChangeUserPassword(ChangeUserPasswordDTO changeUserPasswordDTO)
+
+    {
+        var user = _userRepository.GetUserById(changeUserPasswordDTO.UserId);
+        if (user is null)
+        { 
+            // throw exception
+        }
+
+        user.ChangePassword(changeUserPasswordDTO.Password);
+        _userRepository.UpdateUser(user);
+    }
+
+    public void CreateUser(UserCreationDTO userCreationDTO)
+    {
+        var user = new User(userCreationDTO.Login, userCreationDTO.Password);
+        _userRepository.CreateUser(user);
+    }
+
+    public void RemoveRolesToUser(RemoveRolesFromUser removeRolesFromUserDTO)
+    {
+        var user = _userRepository.GetUserById(removeRolesFromUserDTO.UserId);
+        if (user is null)
+        {
+            // throw exception
+        }
+
+        var roles = _roleRepository.GetAllRoles()
+            .Where(role => removeRolesFromUserDTO.RolesIds.Contains(role.Id))
+            .ToList();
+        if (roles.Count != removeRolesFromUserDTO.RolesIds.Count)
+        {
+            // throw exception
+        }
+
+        user.AddRoles(roles);
+        _userRepository.UpdateUser(user);
+    }
+}
