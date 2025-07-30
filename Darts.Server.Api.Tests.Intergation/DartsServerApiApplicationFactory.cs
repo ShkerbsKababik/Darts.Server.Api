88dbed125d5fd762ce40dc131c154ec37b46d4d4
Vladimir <110543788+ShkerbsKababik@users.noreply.github.com>
2025-07-30 12:32:37 +0300

    add xunit tests and configure inmemory db for it

---
+++ Darts.Server.Api.Tests.Intergation/DartsServerApiApplicationFactory.cs
@@ -0,0 +1,29 @@
+﻿using Darts.Server.Infrastructure.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Darts.Server.Api.Tests.Intergation;
+
+public class DartsServerApiApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            // Remove all DbContext-related registrations
+            var descriptors = services.Where(
+                d => d.ServiceType.Name.Contains("DbContext")).ToList();
+
+            foreach (var descriptor in descriptors)
+            {
+                services.Remove(descriptor);
+            }
+
+            // Add in-memory database
+            services.AddDbContext<DartsDbContext>(options =>
+                options.UseInMemoryDatabase("TestDb"));
+        });
+    }
+}
---
+++ Darts.Server.Api.Tests.Intergation/UserServiceIntergrationTests.cs
@@ -0,0 +1,33 @@
+﻿using Darts.Server.Application.DTO;
+using System.Net.Http.Json;
+
+namespace Darts.Server.Api.Tests.Intergation;
+
+public class UserServiceIntergrationTests : IClassFixture<DartsServerApiApplicationFactory<Program>>
+{
+    private readonly HttpClient _httpClient;
+
+    public UserServiceIntergrationTests(DartsServerApiApplicationFactory<Program> factory)
+    {
+        _httpClient = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task UserCreationTest()
+    {
+        var login = $"user{Guid.NewGuid()}";
+        var password = $"password{Guid.NewGuid()}";
+
+        var userCreationDTO = new UserCreationDTO()
+        {
+            Login = login,
+            Password = password
+        };
+
+        await _httpClient.PostAsJsonAsync("/user/add", userCreationDTO);
+        var users = await _httpClient.GetFromJsonAsync<List<UserDTO>>($"/users");
+
+        Assert.NotNull(users);
+        Assert.Contains(users, u => u.Login == login);
+    }
+}
